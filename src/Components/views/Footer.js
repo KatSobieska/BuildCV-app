0c1709777c957d18c3bc25e0ba2e9b56f108c737
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2023-08-20 20:24:32 +0200

    Add Footer

---
+++ src/Components/views/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div
+      className="d-flex fixed-bottom justify-content-center"
+      style={{ color: "gray" }}
+    >
+      Copyright © BuildCV app 2023 All rights reserved
+    </div>
+  );
+};
+
+export default Footer;
