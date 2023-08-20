62475cf8b34936af144ca26630f97be63ddfd1d0
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2023-08-20 20:26:08 +0200

    Add Footer and Info to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router";
 import Formcv from "./Components/features/Formcv";
 import Home from "./Components/pages/Home";
+import Info from "./Components/pages/Info";
 import Header from "./Components/views/Header";
+import Footer from "./Components/views/Footer";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/form" element={<Formcv />} />
+        <Route path="/info" element={<Info />} />
       </Routes>
+      <Footer />
     </Container>
   );
 }
