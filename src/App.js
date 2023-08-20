83b41f189b8b7561878bee5050f0ac4affa8d332
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2023-08-20 13:28:01 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router";
 import Formcv from "./Components/features/Formcv";
+import Home from "./Components/pages/Home";
 import Header from "./Components/views/Header";
 
 function App() {
   return (
     <Container>
       <Header />
-      <Formcv />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/form" element={<Formcv />} />
+      </Routes>
     </Container>
   );
 }
